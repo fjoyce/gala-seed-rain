3ce7e2557319aaa2cb450814fe0351c5d8e3f5ec
Francis Joyce <fjoyce@ucsc.edu>
2021-06-21 20:11:31 -0600

    change to column graph of mean & SE, fix y-axis label

--- boxWhiskerGenerate.R
+++ boxWhiskerGenerate.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(plotrix)
 
 boxWhiskerCreate <- function(data_input, succession_stage_id, growth_form_id, dispersal_mode_id ){
   
@@ -20,9 +21,6 @@ boxWhiskerCreate <- function(data_input, succession_stage_id, growth_form_id, di
   #They make the graph extremely small making it hard to see any trends, we filter deposition rate below 10 to better see trends
   #processed_data <- processed_data %>%
    # filter(DepositionRate < 10)
-  
-
- 
   
   if (succession_stage_id != 'All') {
     processed_data <- processed_data %>% 
@@ -50,11 +48,25 @@ boxWhiskerCreate <- function(data_input, succession_stage_id, growth_form_id, di
       labels=c('Nat_reg', 'App_nuc', 'Plantation', 'Ref_for')
     ))
   
-  p <- ggplot(processed_data, aes(x=Treatment, y=log10(Seed_deposition_rate))) + 
-    geom_boxplot()+
-    geom_boxplot(fill='#A4A4A4', color="black")+
-    theme_classic()+ 
-    ylab('Deposition rate (m-2 yr-1')+
+  mean_se <- processed_data %>% 
+    group_by(Treatment) %>% 
+    summarize(mean_seeds = mean(Seed_deposition_rate), SE =std.error(Seed_deposition_rate))
+  
+  
+  p <- ggplot(mean_se, aes(x=Treatment, y=mean_seeds)) + 
+    geom_col()+
+    geom_col(fill='#368000', color="#368000")+
+    geom_errorbar(aes(ymin=mean_seeds-SE, 
+                      ymax=mean_seeds+SE))+
+    #theme_classic()+
+    scale_y_log10(breaks = function(x) {brks <- extended_breaks(Q = c(1, 5))(log10(x))
+      10^(brks[brks %% 1 == 0])
+    },
+    labels = math_format(format = log10))+
+    
+    
+    
+    ylab(expression(Deposition~rate~(m^-2~yr^-1)))+
     xlab('Restoration treatment')
   
  return(p)
