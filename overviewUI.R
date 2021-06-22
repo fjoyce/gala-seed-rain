43ce3527e2e6b005d8dee13775775d95b5a11518
Francis Joyce <fjoyce@ucsc.edu>
2021-06-21 18:26:52 -0600

    delete overview tab content

--- overviewUI.R
+++ overviewUI.R
@@ -1,36 +1,5 @@
 overviewTab <- tabItem(
   tabName='overview',
   fluidRow(
-    h1('Introduction'),
-    img(src = "https://www.learngala.com/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaUlTIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--77f4ae89098bf22b3c22d17cacc70d4d00ad1a34/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDam9LYzNSeWFYQlVPaFJ6WVcxd2JHbHVaeTFtWVdOMGIzSkpJZ28wT2pJNk1BWTZCa1ZVT2d4eGRXRnNhWFI1U1NJSE9EVUdPd2RVT2c5amIyeHZjbk53WVdObFNTSUpjMUpIUWdZN0IxUTZEbWx1ZEdWeWJHRmpaVWtpQ1d4cGJtVUdPd2RVIiwiZXhwIjpudWxsLCJwdXIiOiJ2YXJpYXRpb24ifX0=--d7e344ce9a725edeb7e3d7ba25531a445ae49f2f/Site%20change%20over%20time.jpg", height = 368, width = 668),
-  
-   
-   
-    h5(  
     
-'
-On the last page you saw the pattern of large animal-dispersed tree seds that were dispersed into each treatment. Here, you will have the opportunity to 
-  visualize the dispersal of different categories of seeds using data from the sites. We collected the seeds in seed traps and then counted them twice monthly
-  for 3.5 years. The seeds fall into different categories. 
-'),
-
-h5('   
-  Successional stage: Early-successional species are better adapted to disturbed conditions and colonize 
-  early in the recovery process, whereas later-successional species tend to colonize more slowly and are found in mature tropical forest. 
-  Growth form: We collected seeds of both trees and shrubs.
-  '),
-
-h5( '
-  
-	Dispersal mode: Many trees and shrubs in tropical forests are dispersed by animals, whereas some are dispersed by wind. A few are dispersed by water or other 
-  methods but those are not included as separate categories here.
-  '),
-
-h5( '
-Develop a hypothesis about how the dispersal of a certain group of species (e.g. animal-dispersed shrubs) varies across treatments. For example, how do you
-think that the number of seeds of wind-dispersed shrubs would differ across treatments and why? Then you can use the drop down menus on the next page to 
-visualize the patterns of seed deposition. A quick hint that in general later-successional trees have more large, animal-dispersed seeds, whereas there are
-many shrubs and early-successional trees with small, animal-dispersed seeds. There are a few wind-dispersed species in each group. 
-' 
- )
 ))
