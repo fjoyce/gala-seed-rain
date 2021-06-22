4ff57c74098f2f7ea97f88894fb405e86cdb6d16
Francis Joyce <fjoyce@ucsc.edu>
2021-06-21 20:10:16 -0600

    get rid of overview menu item and app header title

--- ui.R
+++ ui.R
@@ -1,27 +1,27 @@
 library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
+library(scales)
 
 source('overviewUI.R')
 source('boxWhiskerUI.R')
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem('Overview', tabName='overview'),
+    #menuItem('Overview', tabName='overview'),
     menuItem('Seed dispersal visualization', tabName='boxWhiskers')
-  )
+ )
 )
 
 body <- dashboardBody(
   
   tabItems(
-    overviewTab,
     boxWhiskerTab
   )
 )
 
 dashboardPage(
-  dashboardHeader(title='Restoring tropical forests'),
+  dashboardHeader(title=''),
   sidebar,
   body,
   skin='green'  
