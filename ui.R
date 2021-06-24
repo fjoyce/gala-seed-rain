8c5af5ec24343063748c5977cc0fb6cd603119fc
Francis Joyce <fjoyce@ucsc.edu>
2021-06-23 19:08:57 -0600

    Change to Sidebarlayout, put inputs and instructions into the sidepanel

--- ui.R
+++ ui.R
@@ -1,52 +1,79 @@
 library(shiny)
-library(shinydashboard)
-library(shinyWidgets)
+#library(shinydashboard)
+#library(shinyWidgets)
 library(scales)
 
-source('overviewUI.R')
-source('boxWhiskerUI.R')
+####New layout should basically be the following#### 
+#####(without the ui object since this is a 2-file app)#####
 
-sidebar <- dashboardSidebar(
-  sidebarMenu(
-    #menuItem('Overview', tabName='overview'),
-    menuItem('Seed dispersal visualization', tabName='boxWhiskers')
- )
+fluidPage(
+  
+  titlePanel("Seed Deposition by Restoration Treatment"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(width = 3,
+      # box(
+      #   h1('Seed Deposition'),
+      #   'Use the dropdown menu to see the patterns
+      # of seed dispersal by different categories.
+      # Select "All" to gain an understanding
+      # of the overall pattern or specify a category to test
+      # your hypothesis.',
+      tags$p("Use the dropdown menus to see the patterns of seed deposition by different categories of seeds. Select 'All' to gain an understanding of the overall pattern or specify particular categories to test your hypothesis."),
+      
+      selectInput(inputId='succession_stage_id',
+                  label='Successional Stage ',
+                  choices=c( 'All', 'Early', 'Both', 'Late'),
+                  selected=c('All')),
+      selectInput(inputId='growth_form_id',
+                  label= 'Growth form',
+                  choices=c('All', 'Tree','Shrub'),
+                  selected=c('All')),
+      selectInput(inputId='dispersal_mode_id',
+                  label=  'Dispersal mode',
+                  choices=c('All', 'Animal-dispersed' , 'Wind-dispersed'),
+                  selected=c('All'))
+    ),
+  
+    mainPanel(width = 9,
+      plotOutput('colPlotRender'))
+      
+)
 )
 
-body <- dashboardBody(
-  
-  tabItems(
-    boxWhiskerTab
-  )
-)
+#############################
 
-dashboardPage(
-  dashboardHeader(title=''),
-  sidebar,
-  body,
-  skin='green'  
-  
-  
-  )
+#source('overviewUI.R')
+#source('boxWhiskerUI.R')
 
 
-#New layout should basically be the following 
-#(without the ui object since this is a 2-file app)
-# ui <- fluidPage(
+##This one shouldn't be needed since 
+##I put those UI elements directly into the sidebar##
+#source('colPlotUI.R')
+
+# sidebar <- dashboardSidebar(
+#   sidebarMenu(
+#     #menuItem('Overview', tabName='overview'),
+#     menuItem('Seed dispersal visualization', tabName='colPlot')
+#  )
+# )
+# 
+# body <- dashboardBody(
 #   
-#   titlePanel("Seed Deposition by Restoration Treatment"),
-#   
-#   sidebarLayout(
-#     
-#     sidebarPanel(
-#       sliderInput(
-#         "bins", label = "Number of bins:",
-#         min = 1, value = 30, max = 50
-#       )
-#     ),
-#     
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
+#   tabItems(
+#     colPlotTab
 #   )
 # )
+# 
+# dashboardPage(
+#   dashboardHeader(title=''),
+#   sidebar,
+#   body,
+#   skin='green'  
+#   
+#   )
+
+
+  #)
+#)
