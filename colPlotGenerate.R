43f90e660b0864501270c353e84b1e4b72bcf483
Francis Joyce <fjoyce@ucsc.edu>
2021-06-25 17:25:51 -0600

    change color, font size, static axis limits, fix y axis label

--- colPlotGenerate.R
+++ colPlotGenerate.R
@@ -57,23 +57,23 @@ colPlotCreate <- function(data_input, succession_stage_id, growth_form_id, dispe
               SE = std.error(trap_dep_rate))
   
   p <- ggplot(mean_se, aes(x = Treatment, y = mean_dep_rate)) +
+    #more muted blue #20618D
     #JAE graph color is #0201FF 
-    geom_col(fill = '#20618D') + 
+    geom_col(fill = '#0201FF') + 
     geom_errorbar(aes(ymin = mean_dep_rate - SE,
                       ymax = mean_dep_rate + SE)) +
-    theme_cowplot()+
-    #theme_classic()+
-    #scale_y_log10(limits = c(0.001,100000))+
+    theme_cowplot(font_size = 20) +
     scale_y_log10(
       breaks = function(x) {
-        brks <- extended_breaks(Q = c(1, 5))(log10(x))
+        brks <- extended_breaks(Q = c(1,5))(log10(x))
         10 ^ (brks[brks %% 1 == 0])
       },
+      limits = c(10^0,10^4),
       labels = math_format(format = log10),
-      #limits = c(0,10000)
     ) +
-    ylab(expression(Deposition ~ rate ~ (m ^ -2 ~ yr ^ -1))) +
-    xlab('Restoration treatment') 
+    ylab(expression(Deposition ~ rate ~ (seeds ~ m ^ -2 ~ yr ^ -1))) +
+    xlab('Restoration treatment') +
+    labs(caption = "Bars show means. Error bars are 1 standard error of the mean.")
   
   return(p)
 }
